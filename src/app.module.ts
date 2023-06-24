d73d321b7f1fe70956cf8c29a7ddd70073c9dda8
Zechariah-dev <omoladesunday2017@gmail.com>
2023-06-24 08:37:16 +0100

    feat: setup redis cachce

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import * as redisStore from "cache-manager-redis-store";
+import { CacheModule } from "@nestjs/cache-manager";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      // store: redisStore,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
