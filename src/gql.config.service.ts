cfc61efddbfccd29c544abc5de43d87e0b6629e5
Zechariah-dev <omoladesunday2017@gmail.com>
2023-07-01 21:30:09 +0100

    fix: make gqlconfigservice injectable

--- src/gql.config.service.ts
+++ src/gql.config.service.ts
@@ -2,17 +2,19 @@ import { ApolloDriverConfig } from "@nestjs/apollo";
 import { ConfigService } from "@nestjs/config";
 import { GqlModuleOptions, GqlOptionsFactory } from "@nestjs/graphql";
 import { GraphqlConfig } from "./common/configs/config.interface";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class GqlConfigService implements GqlOptionsFactory {
   constructor(private configService: ConfigService) {}
   createGqlOptions(): ApolloDriverConfig {
-    // const graphqlConfig = this.configService.get<GraphqlConfig>("graphql");
-    const graphqlConfig = {
-      playgroundEnabled: true,
-      debug: true,
-      schemaDestination: "./src/schema.graphql",
-      sortSchema: true,
-    };
+    const graphqlConfig = this.configService.get<GraphqlConfig>("graphql");
+    // const graphqlConfig = {
+    //   playgroundEnabled: true,
+    //   debug: true,
+    //   schemaDestination: "./src/schema.graphql",
+    //   sortSchema: true,
+    // };
     return {
       // schema options
       autoSchemaFile: graphqlConfig.schemaDestination || "./src/schema.graphql",
